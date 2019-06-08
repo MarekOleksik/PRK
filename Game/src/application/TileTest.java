01bcb185a662efd30b647b0e60d86286e78effbb
Marek <you@example.com>
2019-06-08 13:20:48 +0200

    dodano klasę testującą TileTest

---
+++ Game/src/application/TileTest.java
@@ -0,0 +1,44 @@
+package application;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * Klasa testująca klasę Tile.
+ * 
+ * @author Marek Oleksik
+ */
+public class TileTest {
+	/**
+	 * Metoda testuje, czy pole posiada pionek
+	 */
+	@Test
+	public void tileHasRedPiece() {
+		Piece piece = new Piece(PieceType.RED, 1, 1, 10);
+		Tile tile = new Tile(false, 1, 1, 10, 10);
+		tile.setPiece(piece);
+		assertTrue(tile.hasPiece() == true);
+	}
+	/**
+	 * Metoda testuje, czy pole nie posiada pionka
+	 */
+	@Test
+	public void tileNotHasWhitePiece() {
+		Tile tile = new Tile(true, 1, 1, 10, 10);
+		tile.setPiece(null);
+		assertTrue(tile.hasPiece() == false);
+	}
+	/**
+	 * Metoda testuje getter i setter pionka
+	 */
+	@Test
+	public void getSetPieceTest() {
+		Piece piece = new Piece(PieceType.RED, 1, 1, 10);
+		Tile tile = new Tile(false, 1, 1, 10, 10);
+		tile.setPiece(piece);
+
+		assertTrue(tile.getPiece().equals(piece));
+	}
+}
+
